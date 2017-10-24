e9e54adfe1e61d4cf3a729c440c53b0c0a226f16
Usuario <Usuario@192.168.0.15>
2017-10-24 01:42:13 -0600

    Primer avance de Guia PrimeFaces

--- src/java/Managed/Employeemb.java
+++ src/java/Managed/Employeemb.java
@@ -38,7 +38,7 @@ public class Employeemb {
         generarListaEmpleado();
         
         em.close();
-        return "employee";
+        return "employee.xhtml";
     }
     
     //listar los empleados de la Base de Datos
